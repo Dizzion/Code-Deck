bd25e4ec6d8b3ad4a277f5f5338858713984cc1b
AShaver43 <gofrodude_197@hotmail.com>
2020-09-10 01:01:26 -0700

    missed update

--- CodeDeckAPI/Models/JavaScriptAnswers.cs
+++ CodeDeckAPI/Models/JavaScriptAnswers.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CodeDeckAPI.Models
 {
     public class JavaScriptAnswers
     {
-        
+        [Key]
+        public int Id { get; set; }
+
+        public string JavaScriptAnswer { get; set; }
+        public Answers Answers { get; set; }
+        public int AnswersId { get; set; }
     }
 }
